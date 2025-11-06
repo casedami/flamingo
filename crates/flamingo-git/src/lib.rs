c88023c3d939530d749abc18458974d8107c076a
Casey Miller <cd.miller1230@gmail.com>
2025-11-06 11:36:57 -0700

    test(git): add basic unit tests

--- crates/flamingo-git/src/lib.rs
+++ crates/flamingo-git/src/lib.rs
@@ -220,25 +220,158 @@ fn get_repo_state<P: AsRef<Path>>(path: P) -> GitState {
         _ if exists("BISECT_LOG") => GitState::Bisecting,
         _ => GitState::Clean,
     }
+}
 
-    if git_dir.join("REBASE_HEAD").exists()
-        || git_dir.join("rebase-apply").exists()
-        || git_dir.join("rebase-merge").exists()
-    {
-        return Ok(GitState::Rebasing);
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::test_utils::repo::TestRepo;
+    use flamingo_config::git::{GitConfig, GitSymbols};
+
+    #[test]
+    fn test_dirty_repository() {
+        let mut repo = TestRepo::init();
+        repo.make_dirty();
+
+        let config = GitConfig {
+            format: "$branch$dirty".to_string(),
+            symbols: GitSymbols {
+                dirty: "*".to_string(),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        let result = git(&repo.dir, &config).unwrap();
+        assert_eq!(result, String::from("master*"));
     }
 
-    if git_dir.join("CHERRY_PICK_HEAD").exists() {
-        return Ok(GitState::CherryPicking);
+    #[test]
+    fn test_branch_valid_repo() {
+        let repo = TestRepo::init();
+        let config = GitConfig {
+            format: "$branch".to_string(),
+            ..Default::default()
+        };
+
+        let result = git(&repo.dir, &config);
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), String::from("master"));
     }
 
-    if git_dir.join("REVERT_HEAD").exists() {
-        return Ok(GitState::Reverting);
+    #[test]
+    fn test_branch_invalid_repo() {
+        let config = GitConfig {
+            format: "$branch".to_string(),
+            ..Default::default()
+        };
+
+        let result = git("not/a/git/repo", &config);
+        assert!(result.is_err());
     }
 
-    if git_dir.join("BISECT_LOG").exists() {
-        return Ok(GitState::Bisecting);
+    #[test]
+    fn test_branch_detached_head() {
+        let mut repo = TestRepo::init();
+        let config = GitConfig {
+            format: "$branch".to_string(),
+            ..Default::default()
+        };
+
+        repo.commit("newfile", "content", "Adding new file")
+            .detach_head();
+
+        let result = git(&repo.dir, &config);
+        assert!(result.is_ok());
+        assert!(result.unwrap().contains("HEAD@"));
     }
 
-    Ok(GitState::Clean)
+    #[test]
+    fn test_ahead() {
+        let upstream = TestRepo::init_bare();
+        let mut local = TestRepo::init();
+        let _ = local.set_upstream(&upstream.dir);
+        local.create_commits_ahead(3);
+
+        let config = GitConfig {
+            format: "$ahead$behind".to_string(),
+            symbols: GitSymbols {
+                ahead: "↑".to_string(),
+                behind: "↓".to_string(),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        let result = git(&local.dir, &config).unwrap();
+        assert_eq!(result, String::from("↑3"));
+    }
+
+    #[test]
+    fn test_behind() {
+        let upstream = TestRepo::init_bare();
+
+        let mut temp = TestRepo::init();
+        let _ = temp.set_upstream(&upstream.dir);
+        temp.commit("initial.txt", "initial", "Initial commit")
+            .push();
+
+        let mut local = TestRepo::clone(&upstream.dir);
+
+        temp.create_commits_ahead(3).push();
+        local.fetch_remote();
+
+        let config = GitConfig {
+            format: "$ahead$behind".to_string(),
+            symbols: GitSymbols {
+                ahead: "↑".to_string(),
+                behind: "↓".to_string(),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+        let result = git(&local.dir, &config).unwrap();
+        assert_eq!(result, String::from("↓3"));
+    }
+
+    #[test]
+    fn test_stash_count() {
+        let mut repo = TestRepo::init();
+        repo.add_to_stash().add_to_stash();
+
+        let config = GitConfig {
+            format: "$stash".to_string(),
+            ..Default::default()
+        };
+
+        let result = git(&repo.dir, &config).unwrap();
+        assert_eq!(result, String::from("$2"));
+    }
+
+    #[test]
+    fn test_detached_head() {
+        let mut repo = TestRepo::init();
+        repo.create_commits_ahead(2).detach_head();
+
+        let config = GitConfig {
+            format: "$branch".to_string(),
+            ..Default::default()
+        };
+        let result = git(&repo.dir, &config).unwrap();
+        assert!(result.contains("HEAD@"));
+    }
+
+    #[test]
+    fn test_merge_state() {
+        let mut repo = TestRepo::init();
+        repo.create_merge_conflict();
+
+        let config = GitConfig {
+            format: "$branch $state".to_string(),
+            ..Default::default()
+        };
+
+        let result = git(&repo.dir, &config).unwrap();
+        assert!(result.contains("MERGING"));
+    }
 }
